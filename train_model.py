4d99f0d206abecf991ca8af71395b2c0d263cde7
michal <oolejnikmichal@gmail.com>
2024-04-10 20:30:30 +0200

    model training integrated with neptune.ai

--- train_model.py
+++ train_model.py
@@ -3,10 +3,10 @@ import pandas as pd
 import pickle
 
 # import model training libraries
-from utils import create_predictors
 from skforecast.model_selection import grid_search_forecaster
 from skforecast.ForecasterAutoregCustom import ForecasterAutoregCustom
 from sklearn.ensemble import RandomForestRegressor
+from skforecast.ForecasterAutoreg import ForecasterAutoreg
 
 # imports for data reading
 import requests
@@ -26,7 +26,7 @@ steps = 36
 n_datos_entrenar = 200
 path_fichero = 'bitcoin.csv'
 path_modelo = 'model.pickle'
-uri_mlflow = 'http://104.198.136.57:8080/'
+#uri_mlflow = 'http://104.198.136.57:8080/' no need to use currently ?
 experiment_name = "bictoin_transactions"
 
 # Extract info from Bitcoin
@@ -46,19 +46,19 @@ data.columns = ['date', 'transactions'] # way better than df.rename(XXXXX)
 data['date'] = data['date'].dt.round('H')
 grouped_data = data.groupby('date').sum().reset_index()
 
-# I put data as needed for prediction
+
+# additional frequency settings missed from original neptune.ai code
 grouped_data = grouped_data.set_index('date')
-grouped_data = grouped_data['transactions']
+grouped_data = grouped_data.asfreq("H")
 
 # Train test split
 train_data = grouped_data[ -n_datos_entrenar:-steps]
 test_data  = grouped_data[-steps:]
 
 # Define forecaster
-forecaster_rf = ForecasterAutoregCustom(
+forecaster_rf = ForecasterAutoreg(
                     regressor      = RandomForestRegressor(random_state=123),
-                    fun_predictors = create_predictors,
-                    window_size    = 20
+                    lags    = 20
                 )
 
 # Define grid search
@@ -66,10 +66,9 @@ param_grid = { 'n_estimators': [100, 500], 'max_depth': [3, 5, 10] }
 
 grid_results = grid_search_forecaster(
                         forecaster  = forecaster_rf,
-                        y           = train_data,
+                        y           = train_data["transactions"],
                         param_grid  = param_grid,
                         steps       = 10,
-                        method      = 'cv',
                         metric      = 'mean_squared_error',
                         initial_train_size    = int(len(train_data)*0.5),
                         allow_incomplete_fold = True,
@@ -78,6 +77,7 @@ grid_results = grid_search_forecaster(
                     )
 
 # Upload metadata to Neptune
+# we will make as many RUNS in neptune app as there are results (grid result models)
 for i in range(grid_results.shape[0]):
 
   run = neptune.init_run(
@@ -87,7 +87,7 @@ for i in range(grid_results.shape[0]):
 
   params = grid_results['params'][i]
   run["parameters"] = params
-  run["mean_squared_error"] = grid_results['metric'][i]
+  run["mean_squared_error"] = grid_results['mean_squared_error'][i]
 
   run.stop()
 
